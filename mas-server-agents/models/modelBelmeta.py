6923225ab7af04dbbf4a5c453099360011e4ad1c
Artem Khodakov <artem.khodakov@exposit.com>
2019-12-11 00:53:25 +0300

    Models: name fixes

--- mas-server-agents/models/modelBelmeta.py
+++ mas-server-agents/models/modelBelmeta.py
@@ -11,12 +11,12 @@ class BelmetaModel(Model):
                     list="//article[contains(@class, 'job')]",
                     parameters=dict(
                         name=".//h2[@class='title']/a/text()",
-                        price=".//div[@class='salary']/text()",
+                        salary=".//div[@class='salary']/text()",
                         company=".//div[@class='company']/text()",
                         location=".//div[@class='region']/text()"
                     ),
                     functions=dict(
-                        price=lambda x: [
+                        salary=lambda x: [
                             " ".join([item.replace("\xa0", "") for item in x])]
                     ),
                     regex=dict()
--- mas-server-agents/models/modelTutBy.py
+++ mas-server-agents/models/modelTutBy.py
@@ -11,12 +11,12 @@ class TutByModel(Model):
                     list="//div[contains(@id, 'HH-React-Root')]/div",
                     parameters=dict(
                         name=".//h1[contains(@data-qa, 'vacancy-title')]/span/text()",
-                        price=".//p[contains(@class, 'vacancy-salary')]/text()[string-length(.)>0]",
+                        salary=".//p[contains(@class, 'vacancy-salary')]/text()[string-length(.)>0]",
                         company=".//a[contains(@itemprop, 'hiringOrganization')]/span[contains(@itemprop, 'name')]/span/text()",
                         location=".//p[span[contains(@itemprop, 'jobLocation')]]/text()"
                     ),
                     functions=dict(
-                        price=lambda x: [
+                        salary=lambda x: [
                             " ".join([item.replace("\xa0", "") for item in x])]
                     ),
                     regex=dict()
@@ -25,14 +25,14 @@ class TutByModel(Model):
                     list="//div[@data-qa='vacancy-serp__vacancy']",
                     parameters=dict(
                         name=".//a[contains(@data-qa, 'vacancy-serp__vacancy-title')]/text()",
-                        price=".//div[contains(@data-qa, 'vacancy-serp__vacancy-compensation')]/text()[string-length(.)>0]",
+                        salary=".//div[contains(@data-qa, 'vacancy-serp__vacancy-compensation')]/text()[string-length(.)>0]",
                         company=".//a[contains(@data-qa, 'vacancy-serp__vacancy-employer')]/text()",
                         location=".//span[contains(@data-qa, 'vacancy-serp__vacancy-address')]/text()"
                     ),
                     functions=dict(
                         name=lambda x: [item.replace(
                             "\xa0", "") for item in x],
-                        price=lambda x: [item.replace(
+                        salary=lambda x: [item.replace(
                             "\xa0", "") for item in x],
                         company=lambda x: [item.replace(
                             "\xa0", "") for item in x],
